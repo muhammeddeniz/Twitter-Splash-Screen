433ba76a219751aa607980004523b3a95442cfcf
muhammeddeniz <muhammed.deniz@bakircay.edu.tr>
2022-02-02 23:56:14 +0300

    intial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 28 }}>Home Screen</Text>
+    </View>
+  );
+}
+export default App;
